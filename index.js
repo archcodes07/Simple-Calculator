7f0864805acc077b8c8955a7ab41c6575b0a1c6b
Darsheel Savla <savlad30@gmail.com>
2021-05-13 23:33:16 +0530

    All calc keys functional

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ const action = key.dataset.action
 if (!action) {
   console.log('number key!')
 }
+
+if (
+  action === 'add' ||
+  action === 'subtract' ||
+  action === 'multiply' ||
+  action === 'divide'
+) {
+  console.log('operator key!')
+}
+
+if (action === 'decimal') {
+  console.log('decimal key!')
+}
+
+if (action === 'clear') {
+  console.log('clear key!')
+}
+
+if (action === 'calculate') {
+  console.log('equal key!')
+}
