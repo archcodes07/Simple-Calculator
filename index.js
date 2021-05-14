711262f62087a71d383e25884c2f3f299462c170
Darsheel Savla <savlad30@gmail.com>
2021-05-14 23:58:53 +0530

    Extra functionality #1

--- index.js
+++ index.js
@@ -34,3 +34,21 @@ if (action === 'clear') {
 if (action === 'calculate') {
   console.log('equal key!')
 }
+
+const display = document.querySelector('.calculator__display')
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    // ...
+  }
+})
+
+if (!action) {
+  if (displayedNum === '0') {
+    display.textContent = keyContent
+  }
+}
